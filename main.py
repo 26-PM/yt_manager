4cff7e45178857fdcaf88123ca91cbe94a51e531
PM <121531128+26-PM@users.noreply.github.com>
2024-04-11 06:50:13 +0000

    added all functions

--- main.py
+++ main.py
@@ -1,21 +1,56 @@
+import json
+ 
 def load_data():
-    pass
+    try:
+        with open("youtube.txt","r") as file:
+            return json.load(file) 
+    except FileNotFoundError : 
+        return []
+    
+def save_data_helper(videos):
+    with open ("youtube.txt",'w') as file:
+        json.dump(videos,file)
+
 def list_all_videos(videos):
-    pass
+    print("\n ")
+    print("*"*99)
+    for index,video in enumerate(videos,start=1):
+        print(f"{index},{video['name']},Duration:{video['duration']}")
+    print("*"*99)
 
-def add_video(video):
-    pass
+def add_video(videos):
+    name=input("Enter name of video -> ")
+    time=input("Enter duration of the video (in minutes) -> ")
+    videos.append({"name":name, "duration":int(time)})
+    save_data_helper(videos)
 
-def update_video(video):
-    pass
+def update_video(videos):
+    list_all_videos(videos)
+    id=int(input("Which Video you want to Update?->"))
+    if (1 <= id <= len(videos)):
+        name = input('Enter new Name -> ')
+        time = input('Enter New Duration -> ')
+        videos[id-1]['name'] = name
+        videos[id-1]['duration'] = int(time)
+        save_data_helper(videos)
+    else:
+        print("Invalid index.")
 
-def delete_video(video):
-    pass
+def delete_video(videos):
+    list_all_videos(videos)
+    id=int(input("Delete which video?->"))
+    if (1<=id<=len(videos)):    
+        videos.pop(id - 1 )
+        save_data_helper(videos)
+    else:
+        print("Invalid index")    
+
+# Driver code  
 
 def main():
     videos=load_data()
     while True:
-        print("Yt Manager")
+        print("\nYt Manager")
         print("1.List all the videos.")
         print("2.Add a video.")
         print("3.Update")
